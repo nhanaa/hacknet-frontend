16c2d668855caf791843d16587770dd0933f99a2
ChanakyaTS <cthirumalasetty@crimson.ua.edu>
2024-09-14 06:25:29 -0400

    ui improvements

--- src/app/onboarding/goal/page.tsx
+++ src/app/onboarding/goal/page.tsx
@@ -2,8 +2,8 @@ import Goal from "@/components/onboarding/Goal";
 
 export default function Page() {
   return (
-    <div className="h-full w-full flex justify-center items-center">
+    <div className="h-full w-full flex justify-center items-center bg-[url('/normal_bg.svg')] bg-no-repeat bg-cover">
       <Goal />
     </div>
-  )
+  );
 }
--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,17 +1,20 @@
-'use client';
+"use client";
 
-import { Button, Text } from '@chakra-ui/react';
-import { useRouter } from 'next/navigation';
+import { Button, Text } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import logo from "../../../public/logo.svg";
 
 export default function Landing() {
   const router = useRouter();
 
   const handleGetStarted = () => {
-    router.push('/onboarding/upload-resume');
+    router.push("/onboarding/upload-resume");
   };
 
   return (
-    <div className="h-full w-full flex flex-col justify-center items-center gap-5">
+    <div className="h-full w-full flex flex-col justify-center items-center gap-5 bg-[url('/landing_bg.png')] bg-no-repeat bg-cover">
+      <Image src={logo} alt="Logo" />
       <Text fontSize="6xl" fontWeight="bold" color="teal">
         Welcome to HackNet
       </Text>
