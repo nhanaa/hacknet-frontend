5013678e371411c13eb8e5dbda20eef34321d3c9
nhanaa <nhanpro7d@gmail.com>
2024-09-14 14:26:13 -0400

    Push

--- src/components/dashboard/DashboardButtons.tsx
+++ src/components/dashboard/DashboardButtons.tsx
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
-import { Button, Stack } from "@chakra-ui/react";
-import { deleteCookie } from "cookies-next";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { Button, Circle, Stack } from '@chakra-ui/react';
+import { deleteCookie } from 'cookies-next';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export default function DashboardButtons() {
   const router = useRouter();
@@ -11,19 +11,42 @@ export default function DashboardButtons() {
   const teamSwiperClick = () => {
     // setLeave(true);
     setTimeout(() => {
-      router.push("/swiper");
+      router.push('/swiper');
     }, 700);
   };
   return (
     <div
       className={`${
-        leave && "animate-fadeOut"
+        leave && 'animate-fadeOut'
       } h-full w-full flex justify-center items-center pr-32 animate-fade`}
     >
-      <Stack className="flex flex-col justify-center items-center" gap={10}>
-        <Button className="w-64" colorScheme="teal" onClick={teamSwiperClick}>
+      <Stack
+        className="flex flex-col justify-center items-center"
+        gap={10}
+      >
+        <Button
+          className="w-64"
+          colorScheme="teal"
+          onClick={teamSwiperClick}
+        >
           Teammate Swiper
         </Button>
+        <div className="relative">
+          <Circle
+            size="2rem"
+            bg="red"
+            color="white"
+            position="absolute"
+            top={-2}
+            right={-2}
+            zIndex={10}
+          >
+            <p>1</p>
+          </Circle>
+          <Button className="w-64" colorScheme="teal">
+            Your Inbox
+          </Button>
+        </div>
         <Button className="w-64" colorScheme="teal">
           Roster Evaluator
         </Button>
@@ -32,8 +55,8 @@ export default function DashboardButtons() {
           colorScheme="teal"
           variant="outline"
           onClick={() => {
-            deleteCookie("accessToken");
-            router.push("/auth/login");
+            deleteCookie('accessToken');
+            router.push('/auth/login');
           }}
         >
           Logout
